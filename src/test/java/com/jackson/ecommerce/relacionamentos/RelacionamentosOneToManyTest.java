b91e50bac8ab360dc343d219c97cc71be14522f9
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-04 14:14:56 -0300

    4.4 Mapeamento de relacionamento entre entidades

---
+++ src/test/java/com/jackson/ecommerce/relacionamentos/RelacionamentosOneToManyTest.java
@@ -0,0 +1,34 @@
+package com.jackson.ecommerce.relacionamentos;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class RelacionamentosOneToManyTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setCliente(cliente);
+        pedido.setTotal(new BigDecimal(10));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assertions.assertFalse(clienteVerificacao.getPedidos().isEmpty());
+    }
+
+
+}
