87f6ed673efe2b8fd5cc7ad5a67ca59c816175d9
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-04 10:35:20 -0300

    3.10 Usando o modelo de criaçao IDENTITY

--- src/main/java/com/jackson/ecommerce/model/NotaFiscal.java
+++ src/main/java/com/jackson/ecommerce/model/NotaFiscal.java
@@ -1,9 +1,6 @@
 package com.jackson.ecommerce.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -15,6 +12,7 @@ public class NotaFiscal {
 
     @EqualsAndHashCode.Include
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "pedido_id")
