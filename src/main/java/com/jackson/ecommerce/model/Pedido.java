8d458f86341ce1b1344dbffda2bbc046dd78cb43
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-04 18:46:46 -0300

    4.9 Mapeamento relacionamento um-para-um

--- src/main/java/com/jackson/ecommerce/model/Pedido.java
+++ src/main/java/com/jackson/ecommerce/model/Pedido.java
@@ -36,6 +36,9 @@ public class Pedido {
     @Enumerated(EnumType.STRING)
     private StatusPedido status;
 
+    @OneToOne(mappedBy = "pedido")
+    private PagamentoCartao pagamento;
+
     @Embedded
     private EnderecoEntregaPedido enderecoEntrega;
 
