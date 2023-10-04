46375e8ebab7a4c91739b6294f4e1692b5104e8c
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-04 18:46:56 -0300

    4.9 Mapeamento relacionamento um-para-um

---
+++ src/test/java/com/jackson/ecommerce/relacionamentos/RelacionamentosOneToOneTest.java
@@ -0,0 +1,30 @@
+package com.jackson.ecommerce.relacionamentos;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class RelacionamentosOneToOneTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        PagamentoCartao pagamentoCartao = new PagamentoCartao();
+        pagamentoCartao.setNumero("1234");
+        pagamentoCartao.setStatus(StatusPagamento.PROCESSANDO);
+        pagamentoCartao.setPedido(pedido);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pagamentoCartao);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assertions.assertNotNull(pedidoVerificacao.getPagamento());
+    }
+}
