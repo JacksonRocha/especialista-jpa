6bebcfa4dfc31ddae27702a69f9b0a20bbd6bd2e
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-04 17:51:14 -0300

    4.6 Fazendo o autorelacionamento dentro da mesma entidade

---
+++ src/test/java/com/jackson/ecommerce/relacionamentos/AutoRelacionamentosTest.java
@@ -0,0 +1,38 @@
+package com.jackson.ecommerce.relacionamentos;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class AutoRelacionamentosTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento() {
+        Categoria categoriaPai = new Categoria();
+        categoriaPai.setNome("Eletrônicos");
+
+        Categoria categoria = new Categoria();
+        categoria.setNome("Celulares");
+        categoria.setCategoriaPai(categoriaPai);
+
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoriaPai);
+        entityManager.persist(categoria);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assertions.assertNotNull(categoriaVerificacao.getCategoriaPai());
+
+        Categoria categoriaPaiVerificacao = entityManager.find(Categoria.class, categoriaPai.getId());
+        Assertions.assertFalse(categoriaPaiVerificacao.getCategorias().isEmpty());
+    }
+}
+
+
