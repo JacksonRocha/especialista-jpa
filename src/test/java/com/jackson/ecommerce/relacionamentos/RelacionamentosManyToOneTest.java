b30f665de00eb2f300143269bb963d5ebb478512
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-04 14:14:47 -0300

    4.4 Mapeamento de relacionamento entre entidades

--- src/test/java/com/jackson/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
+++ src/test/java/com/jackson/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
@@ -1,9 +1,7 @@
 package com.jackson.ecommerce.relacionamentos;
 
 import com.jackson.ecommerce.EntityManagerTest;
-import com.jackson.ecommerce.model.Cliente;
-import com.jackson.ecommerce.model.Pedido;
-import com.jackson.ecommerce.model.StatusPedido;
+import com.jackson.ecommerce.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +28,34 @@ public class RelacionamentosManyToOneTest extends EntityManagerTest {
 
         Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
         Assertions.assertNotNull(pedidoVerificacao.getCliente());
+    }
+
+    @Test
+    public void verificandoRelacionamentoItemPedido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.persist(itemPedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+        Assertions.assertNotNull(itemPedidoVerificacao.getPedido());
+        Assertions.assertNotNull(itemPedidoVerificacao.getProduto());
     }
 }
