dc02cc4279e007aa8f899f3d7e6d435abbb4cae2
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-05 10:21:21 -0300

    4.10 Mapeamento relacionamento um-para-um

--- src/main/java/com/jackson/ecommerce/model/Pedido.java
+++ src/main/java/com/jackson/ecommerce/model/Pedido.java
@@ -28,8 +28,8 @@ public class Pedido {
     @Column(name = "data_conclusao")
     private LocalDateTime dataConclusao;
 
-    @Column(name = "nota_fiscal_idd")
-    private Integer notaFiscalId;
+    @OneToOne(mappedBy = "pedido")
+    private NotaFiscal notaFiscal;
 
     private BigDecimal total;
 
