2f5206605de875b21a059dae9689b63ef05e2743
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-05 20:36:03 -0300

    5.7 Listeners para eventos do ciclo de vida

---
+++ src/main/java/com/jackson/ecommerce/listener/GerarNotaFiscalListener.java
@@ -0,0 +1,19 @@
+package com.jackson.ecommerce.listener;
+
+import com.jackson.ecommerce.model.Pedido;
+import com.jackson.ecommerce.serive.NotaFiscalService;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+public class GerarNotaFiscalListener {
+
+    private NotaFiscalService notaFiscalService = new NotaFiscalService();
+
+    @PrePersist
+    @PreUpdate
+    public void gerar(Pedido pedido) {
+        if (pedido.isPago() && pedido.getNotaFiscal() == null) {
+            notaFiscalService.gerar(pedido);
+        }
+    }
+}
