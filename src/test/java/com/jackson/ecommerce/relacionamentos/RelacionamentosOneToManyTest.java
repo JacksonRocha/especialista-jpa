60d28c68e8beb3bf63161edf09ccf6d7cb88e585
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-05 20:22:18 -0300

    5.6 Callbacks para eventos do ciclo de vida

--- src/test/java/com/jackson/ecommerce/relacionamentos/RelacionamentosOneToManyTest.java
+++ src/test/java/com/jackson/ecommerce/relacionamentos/RelacionamentosOneToManyTest.java
@@ -16,7 +16,7 @@ public class RelacionamentosOneToManyTest extends EntityManagerTest {
 
         Pedido pedido = new Pedido();
         pedido.setStatus(StatusPedido.AGUARDANDO);
-        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setDataCriacao(LocalDateTime.now());
         pedido.setCliente(cliente);
         pedido.setTotal(new BigDecimal(10));
 
@@ -36,7 +36,7 @@ public class RelacionamentosOneToManyTest extends EntityManagerTest {
 
         Pedido pedido = new Pedido();
         pedido.setStatus(StatusPedido.AGUARDANDO);
-        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setDataCriacao(LocalDateTime.now());
         pedido.setTotal(BigDecimal.TEN);
         pedido.setCliente(cliente);
 
