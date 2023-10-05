d9ecfb2a4a17046ee0fdd2530fc14f2aba28840e
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-05 13:11:19 -0300

    4.13 Atributo optinal

---
+++ src/test/java/com/jackson/ecommerce/relacionamentos/OptionalTest.java
@@ -0,0 +1,14 @@
+package com.jackson.ecommerce.relacionamentos;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.Pedido;
+import org.junit.jupiter.api.Test;
+
+public class OptionalTest extends EntityManagerTest {
+
+    @Test
+    public void verificarComportamento() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+    }
+}
