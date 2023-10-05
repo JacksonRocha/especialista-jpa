bad9381551c7e1a35e7cddc815b64eac88feec20
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-05 20:48:16 -0300

    5.7 Listeners para eventos do ciclo de vida

--- src/main/java/com/jackson/ecommerce/model/Produto.java
+++ src/main/java/com/jackson/ecommerce/model/Produto.java
@@ -1,5 +1,7 @@
 package com.jackson.ecommerce.model;
 
+import com.jackson.ecommerce.listener.GenericoListener;
+import com.jackson.ecommerce.listener.GerarNotaFiscalListener;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,6 +10,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Data
+@EntityListeners({ GenericoListener.class})
 @Entity
 @Table(name = "produto")
 public class Produto {
