ae5669be07c9bc12193881306109feec22a3112e
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-05 15:12:18 -0300

    5.1 Estados e ciclos de vida dos objetos

---
+++ src/test/java/com/jackson/ecommerce/conhecendoentitymanager/EstadosECiclosDeVidaTest.java
@@ -0,0 +1,22 @@
+package com.jackson.ecommerce.conhecendoentitymanager;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.Categoria;
+import org.junit.jupiter.api.Test;
+
+public class EstadosECiclosDeVidaTest extends EntityManagerTest {
+
+    @Test
+    public void analisarEstados() {
+        Categoria categoriaNovo = new Categoria();
+
+        Categoria categoriaGerenciadaMerge = entityManager.merge(categoriaNovo);
+
+        Categoria categoriaGerenciada = entityManager.find(Categoria.class, 1);
+
+        entityManager.remove(categoriaGerenciada);
+        entityManager.persist(categoriaGerenciada);
+
+        entityManager.detach(categoriaGerenciada);
+    }
+}
