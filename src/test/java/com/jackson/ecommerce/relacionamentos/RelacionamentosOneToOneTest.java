9b84ea060b010e9eea60e89d18e49f83c24be419
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-05 10:21:35 -0300

    4.10 Mapeamento relacionamento um-para-um

--- src/test/java/com/jackson/ecommerce/relacionamentos/RelacionamentosOneToOneTest.java
+++ src/test/java/com/jackson/ecommerce/relacionamentos/RelacionamentosOneToOneTest.java
@@ -5,7 +5,9 @@ import com.jackson.ecommerce.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Date;
 
 public class RelacionamentosOneToOneTest extends EntityManagerTest {
 
@@ -24,7 +26,27 @@ public class RelacionamentosOneToOneTest extends EntityManagerTest {
 
         entityManager.clear();
 
-        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, 1);
         Assertions.assertNotNull(pedidoVerificacao.getPagamento());
     }
+
+    @Test
+    public void verificarRelacionamentoPedidoNotaFiscal() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        NotaFiscal notaFiscal = new NotaFiscal();
+        notaFiscal.setXml("TESTE");
+        notaFiscal.setDataEmissao(new Date());
+        notaFiscal.setPedido(pedido);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(notaFiscal);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assertions.assertNotNull(pedidoVerificacao.getNotaFiscal());
+    }
+
 }
