64b65598c62c387b32333f32b7a49c24cbedc723
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-06 16:15:22 -0300

    6.5 Mapeando chave composta com @EmbeddedId melhor forma

--- src/main/java/com/jackson/ecommerce/model/ItemPedidoId.java
+++ src/main/java/com/jackson/ecommerce/model/ItemPedidoId.java
@@ -1,5 +1,8 @@
 package com.jackson.ecommerce.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,12 +13,15 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Embeddable
 public class ItemPedidoId implements Serializable {
 
     @EqualsAndHashCode.Include
+    @Column(name = "pedido_id")
     private Integer pedidoId;
 
     @EqualsAndHashCode.Include
+    @Column(name = "produto_id")
     private Integer produtoId;
 
 
