9fafbaf8596b7eac441511f021a8d6a297aaf30b
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-06 20:03:26 -0300

    6.18 Mapeando herança com estratégia de uma tabela por classe (table per class)

--- src/main/java/com/jackson/ecommerce/model/PagamentoCartao.java
+++ src/main/java/com/jackson/ecommerce/model/PagamentoCartao.java
@@ -5,9 +5,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@DiscriminatorValue("cartao")
+//@DiscriminatorValue("cartao")
 @Entity
-//@Table(name = "pagamento_cartao")
+@Table(name = "pagamento_cartao")
 public class PagamentoCartao extends Pagamento{
 
     @Column(name = "numero_cartao")
