14fb7419d98a3f508759e087ef18a248b4537dde
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-06 19:10:20 -0300

    6.15 Mapeando herança com @MappedSuperclass

---
+++ src/main/java/com/jackson/ecommerce/model/EntidadeBaseInteger.java
@@ -0,0 +1,18 @@
+package com.jackson.ecommerce.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@MappedSuperclass
+public class EntidadeBaseInteger {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
