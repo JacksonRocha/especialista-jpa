8542b409e4c2441ab1e447de6d81975aecda6354
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-06 18:09:43 -0300

    6.12 Mepeando e persisitindo dados de arquivos com @Lob

---
+++ src/test/java/com/jackson/ecommerce/mapeamentoavancado/SalvarArquivosTest.java
@@ -0,0 +1,58 @@
+package com.jackson.ecommerce.mapeamentoavancado;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.Cliente;
+import com.jackson.ecommerce.model.NotaFiscal;
+import com.jackson.ecommerce.model.Pedido;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+
+public class SalvarArquivosTest extends EntityManagerTest {
+
+    @Test
+    public void salvarXmlNota() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        NotaFiscal notaFiscal = new NotaFiscal();
+        notaFiscal.setPedido(pedido);
+        notaFiscal.setDataEmissao(new Date());
+        notaFiscal.setXml(carregarNotaFiscal());
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(notaFiscal);
+        entityManager.getTransaction().commit();
+
+        NotaFiscal notaFiscalVerificacao = entityManager.find(NotaFiscal.class, notaFiscal.getId());
+        Assertions.assertNotNull(notaFiscalVerificacao.getXml());
+        Assertions.assertTrue(notaFiscalVerificacao.getXml().length > 0);
+
+/*
+        try {
+            OutputStream out = new FileOutputStream(
+                    Files.createFile(Paths.get(
+                            System.getProperty("user.home") + "/xml.xml")).toFile());
+            out.write(notaFiscalVerificacao.getXml());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+ */
+
+    }
+
+    private static byte[] carregarNotaFiscal() {
+        try {
+            return SalvarArquivosTest.class.getResourceAsStream(
+                    "/nota-fiscal.xml").readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
