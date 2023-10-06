8acf285fce4d1e04509e40a30a9a9c000de28082
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-06 20:03:11 -0300

    6.18 Mapeando herança com estratégia de uma tabela por classe (table per class)

--- src/main/java/com/jackson/ecommerce/model/Pagamento.java
+++ src/main/java/com/jackson/ecommerce/model/Pagamento.java
@@ -4,11 +4,11 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
-@DiscriminatorColumn(name = "tipo_pagamento",
-        discriminatorType = DiscriminatorType.STRING)
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@DiscriminatorColumn(name = "tipo_pagamento",
+//        discriminatorType = DiscriminatorType.STRING)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Entity
-@Table(name = "pagamento")
+//@Table(name = "pagamento")
 public abstract class Pagamento extends EntidadeBaseInteger{
 
     @MapsId
