0ab621f28bb90c7b5252d4823358b293eec2cca6
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-07 17:27:01 -0300

    7.6 Corrigindo os testes do JUnit

--- src/main/java/com/jackson/ecommerce/model/PagamentoCartao.java
+++ src/main/java/com/jackson/ecommerce/model/PagamentoCartao.java
@@ -9,6 +9,6 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class PagamentoCartao extends Pagamento{
 
-    @Column(name = "numero_cartao", length = 50, nullable = false)
+    @Column(name = "numero_cartao", length = 50)
     private String numeroCartao;
 }
