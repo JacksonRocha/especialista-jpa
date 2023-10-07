f5f19db85bcd4fd9d518af61f4391a1cdb91b5d5
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-07 15:29:53 -0300

    7.2 Configurando detalhes da tabela com @Table

---
+++ src/test/java/com/jackson/ecommerce/ddl/DDLTest.java
@@ -0,0 +1,12 @@
+package com.jackson.ecommerce.ddl;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import org.junit.jupiter.api.Test;
+
+public class DDLTest extends EntityManagerTest {
+
+    @Test
+    public void gerarDDL() {
+
+    }
+}
