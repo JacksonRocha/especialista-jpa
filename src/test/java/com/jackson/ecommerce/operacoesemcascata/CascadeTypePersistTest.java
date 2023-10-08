fb7753d66301ddb20043f63d48bb39670562823c
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-08 13:36:05 -0300

    8.4 Fazendo atualizações em cascata

--- src/test/java/com/jackson/ecommerce/operacoesemcascata/CascadeTypePersistTest.java
+++ src/test/java/com/jackson/ecommerce/operacoesemcascata/CascadeTypePersistTest.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
-public class CascadePersistTest extends EntityManagerTest {
+public class CascadeTypePersistTest extends EntityManagerTest {
 
     // @Test
     public void persistirProdutoComCategoria() {
