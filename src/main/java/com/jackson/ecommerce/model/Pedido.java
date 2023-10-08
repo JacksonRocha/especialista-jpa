db55e64af5722a5023eb7642e82a11607e8b35b8
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-08 13:14:41 -0300

    8.2. Fazendo inserções de objetos em cascata

--- src/main/java/com/jackson/ecommerce/model/Pedido.java
+++ src/main/java/com/jackson/ecommerce/model/Pedido.java
@@ -20,7 +20,7 @@ public class Pedido extends EntidadeBaseInteger{
             foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
     private Cliente cliente;
 
-    @OneToMany(mappedBy = "pedido", cascade = CascadeType.PERSIST)
+    @OneToMany(mappedBy = "pedido")
     private List<ItemPedido> itens;
 
     @Column(name = "data_criacao", updatable = false, nullable = false)
