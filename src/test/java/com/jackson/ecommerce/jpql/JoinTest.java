ee2d214732f59ffe48cfae6bad3a72991a6d3dce
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-10 11:54:05 -0300

    9.6 Fazendo inner join entre as entidades

---
+++ src/test/java/com/jackson/ecommerce/jpql/JoinTest.java
@@ -0,0 +1,22 @@
+package com.jackson.ecommerce.jpql;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.Pedido;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class JoinTest extends EntityManagerTest {
+
+    @Test
+    public void fazerJoin() {
+        String jpql = "select p from Pedido p join p.pagamento pag";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertTrue(lista.size() == 3);
+    }
+}
