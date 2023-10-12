82e4775a2953a5897acc1575cf56028aff280245
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-12 12:02:09 -0300

    9.12 Usando expressão condicional like

---
+++ src/test/java/com/jackson/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -0,0 +1,23 @@
+package com.jackson.ecommerce.jpql;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ExpressoesCondicionaisTest extends EntityManagerTest {
+
+    @Test
+    public void usarExpressaoCondicionalLike() {
+        String jpql = "select c from Cliente c where c.nome like :nome";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        typedQuery.setParameter("nome", "Fernando%");
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+    }
+}
