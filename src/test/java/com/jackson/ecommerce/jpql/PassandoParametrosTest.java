5ff40046671d92aaa1d45c2fd40e4b4409bbfa13
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-11 22:32:32 -0300

    9.11 Passando parâmetros para as consultas

--- src/test/java/com/jackson/ecommerce/jpql/PassandoParametrosTest.java
+++ src/test/java/com/jackson/ecommerce/jpql/PassandoParametrosTest.java
@@ -1,16 +1,31 @@
 package com.jackson.ecommerce.jpql;
 
 import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.NotaFiscal;
 import com.jackson.ecommerce.model.Pedido;
 import com.jackson.ecommerce.model.StatusPagamento;
 import com.jackson.ecommerce.model.StatusPedido;
+import jakarta.persistence.TemporalType;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
 import java.util.List;
 
 public class PassandoParametrosTest extends EntityManagerTest {
+
+    @Test
+    public void passarParametroDate() {
+        String jpql = "select nf from NotaFiscal nf where nf.dataEmissao <= ?1";
+
+        TypedQuery<NotaFiscal> typedQuery = entityManager.createQuery(jpql, NotaFiscal.class);
+        typedQuery.setParameter(1, new Date(), TemporalType.TIMESTAMP);
+
+        List<NotaFiscal> lista = typedQuery.getResultList();
+        Assertions.assertTrue(lista.size() == 1);
+    }
+
 
     @Test
     public void passarParametro() {
