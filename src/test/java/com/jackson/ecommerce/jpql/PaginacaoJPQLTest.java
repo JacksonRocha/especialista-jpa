209528845ea35b1bd165138c2eff6da48caf4616
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-12 12:55:54 -0300

    9.20 Fazendo paginação de resultados

---
+++ src/test/java/com/jackson/ecommerce/jpql/PaginacaoJPQLTest.java
@@ -0,0 +1,33 @@
+package com.jackson.ecommerce.jpql;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.Categoria;
+import com.jackson.ecommerce.model.Pedido;
+import com.jackson.ecommerce.model.Produto;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PaginacaoJPQLTest extends EntityManagerTest {
+
+
+    @Test
+    public void paginarResultados() {
+        String jpql = "select c from Categoria c order by c.nome";
+
+        TypedQuery<Categoria> typedQuery = entityManager.createQuery(jpql, Categoria.class);
+
+        // FIRST_RESULT = MAX_RESULTS * (pagina - 1)
+        typedQuery.setFirstResult(6);
+        typedQuery.setMaxResults(2);
+
+        List<Categoria> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
+    }
+}
