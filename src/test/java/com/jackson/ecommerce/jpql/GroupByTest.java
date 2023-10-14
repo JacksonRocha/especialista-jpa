7b187ef8319587855e9a60964188447e4704dea8
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-14 15:51:14 -0300

    9.29 Exercício: usando group by

--- src/test/java/com/jackson/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/jackson/ecommerce/jpql/GroupByTest.java
@@ -15,14 +15,29 @@ public class GroupByTest extends EntityManagerTest {
 //        String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
 
 //         Total de vendas por mês.
-//        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
-//                " from Pedido p " +
-//                " group by year(p.dataCriacao), month(p.dataCriacao) ";
+        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
+                " from Pedido p " +
+                " group by concat(year(p.dataCriacao), '/' , function('monthname', p.dataCriacao))," +
+                " year (p.dataCriacao), month(p.dataCriacao) ";
 
 //         Total de vendas por categoria.
-        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
-                " join ip.produto pro join pro.categorias c " +
-                " group by c.id";
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.produto pro join pro.categorias c " +
+//                " group by c.id";
+
+//        Total de vendas por cliente
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.pedido p join p.cliente c " +
+//                " group by c.id";
+
+////        Total de vendas por dia e por categoria
+//        String jpql = "select " +
+//                " concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
+//                " concat(c.nome, ': ', sum(ip.precoProduto)) " +
+//                " from ItemPedido ip join ip.pedido p join ip.produto pro join pro.categorias c " +
+//                " group by year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao), c.id " +
+//                " order by p.dataCriacao, c.nome ";
+
 
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
