d30f8db54807c14635656dc3919e64f070214b60
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-16 15:31:20 -0300

    9.48 Configurando uma query nomeada com @NamedQuery

---
+++ src/test/java/com/jackson/ecommerce/jpql/NamedQueryTest.java
@@ -0,0 +1,25 @@
+package com.jackson.ecommerce.jpql;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.Pedido;
+import com.jackson.ecommerce.model.Produto;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class NamedQueryTest extends EntityManagerTest {
+
+    @Test
+    public void executarConsulta() {
+        TypedQuery<Produto> typedQuery = entityManager
+                .createNamedQuery("Produto.listarPorCategoria", Produto.class);
+        typedQuery.setParameter("categoria", 2);
+
+        List<Produto> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+}
