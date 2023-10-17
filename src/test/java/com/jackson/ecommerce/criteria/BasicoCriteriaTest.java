59b6ea09d06b9cc3c87c851a7826d6b48d5aca26
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-10-17 17:03:11 -0300

    10.1. Introdução à Criteria API do JPA

---
+++ src/test/java/com/jackson/ecommerce/criteria/BasicoCriteriaTest.java
@@ -0,0 +1,35 @@
+package com.jackson.ecommerce.criteria;
+
+import com.jackson.ecommerce.EntityManagerTest;
+import com.jackson.ecommerce.model.Pedido;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void buscarPorIdentificador() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+
+
+        // String jpql = "select p from Pedido p where p.id = 1";
+
+        TypedQuery<Pedido> typedQuery = entityManager
+                //.createQuery(jpql, Pedido.class);
+                .createQuery(criteriaQuery);
+
+        Pedido pedido = typedQuery.getSingleResult();
+        Assertions.assertNotNull(pedido);
+    }
+}
